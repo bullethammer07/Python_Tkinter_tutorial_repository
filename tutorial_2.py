2f9712d078d2f30b0404daa3084f1485264fb64d
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-12 11:36:42 +0530

    added tut2

---
+++ tutorial_2.py
@@ -0,0 +1,9 @@
+# Below is a simple template that we will be following for tkinter development.
+
+from tkinter import *
+
+root = Tk() # This generates a basic GUI
+
+# GUI logic starts here
+
+root.mainloop()
