125b30067a414d27158e44d5fe043fd824fb5b24
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-16 17:46:23 +0530

    Added resizeable window exercise

---
+++ exercises/resizeable_window_exercise.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------
+# Description : Take Height and Width value from user 
+#               and change the window size
+#-----------------------------------------------------
+
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+
+lbl1 = 
+
+root.mainloop()
