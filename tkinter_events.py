4f6b853729dbe950d7191bd5d3de4e1ba8095ec4
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-16 11:50:54 +0530

    added tkinter events

---
+++ tkinter_events.py
@@ -0,0 +1,32 @@
+#---------------------------------------------------
+#           Events in Tkinter                       
+#---------------------------------------------------
+
+from tkinter import *
+
+root = Tk()
+root.title("Events in Tkinter")
+root.geometry("700x500")
+
+# Making function 'get_val' for button
+def button_beh():
+    pass
+
+# NOTE : The function (below) has been specified with bind.
+#        these functions take the event as the input argument.
+#        below we have specifed the event as 'evt', this can be specified with any name
+def click_beh(evt):
+    # NOTE : We can use the attributes 'x' and 'y' of the event argument specified 'evt'
+    #        the 'x' and 'y' return the coordinates where the click was made
+    print(f"Button Clicked !!... {evt.x , evt.y}")
+
+# Making a button
+# NOTE : here we have specified a color using 'activebackground' for what happens when the button is clicked.
+#        highlight options can be specified using : highlightbackground, highlightcolor and highlightthickness. refer documentation for more details.
+bt1 = Button(root, text="Click Me", padx=10, pady=10, command=button_beh, bg="yellow", activebackground='#696969')
+bt1.pack()
+
+# Binding and event to the widget
+bt1.bind("<Button-1>", func=click_beh)
+
+root.mainloop()
