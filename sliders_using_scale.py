7d2df79844d0d15d1c3ff0cd101af94b68f1de61
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-17 12:35:18 +0530

    added sliders

---
+++ sliders_using_scale.py
@@ -0,0 +1,11 @@
+#-----------------------------
+#    Sliders using Scale()    
+#-----------------------------
+
+from tkinter import *
+
+root = Tk()
+root.title("Sliders")
+root.geometry("700x300")
+
+root.mainloop()
