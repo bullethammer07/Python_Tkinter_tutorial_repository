db3987478bec69b965122e95b64aff09d2f85b1a
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-18 13:33:57 +0530

    added image toggle radio button exercise

---
+++ exercises/radio_button_with_image.py
@@ -0,0 +1,38 @@
+#----------------------------------
+#    Radio Buttons with Image      
+#----------------------------------
+
+from tkinter import *
+
+root = Tk()
+root.geometry("150x50")
+
+def toggle():
+
+    if toggle_btn.config('relief')[-1] == 'sunken':
+        toggle_btn.config(relief="raised")
+    else:
+        toggle_btn.config(relief="sunken")
+
+btn_state = "ON"
+
+on_img = PhotoImage(file=r"./../imgs/button_on_state_small.png")
+off_img = PhotoImage(file=r"./../imgs/button_off_state_small.png")
+
+def click_beh(evt):
+    # print(toggle_btn.config())
+    foo = root.call(toggle_btn.cget('image'), 'cget', '-file')
+    # print(foo)
+    if foo == "./../imgs/button_on_state_small.png":
+        print("Button was ON ... setting it to off")
+        toggle_btn.configure(image=off_img)
+    else:
+        print("Button was OFF ... setting it to ON")
+        toggle_btn.configure(image=on_img)
+        
+toggle_btn = Label(image=on_img)
+toggle_btn.bind("<Button-1>", func=click_beh)
+toggle_btn.pack()
+
+#------------------------------------------------------------------------
+root.mainloop()
